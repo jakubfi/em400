3d9a99fee204a051580371d92a1e2cbf8ad1c607
Jakub Filipowicz <jakubf@gmail.com>
2012-10-11 18:29:31 +0200

    (c) fix

--- dasm/disassm.py
+++ dasm/disassm.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-#  Copyright (c) 2011-2012 Jakub Filipowicz <jakubf@gmail.com>
+#  Copyright (c) 2012 Jakub Filipowicz <jakubf@gmail.com>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
