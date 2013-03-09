329fce514fb3000ec91ea021004b48d85cec246d
Jakub Filipowicz <jakubf@gmail.com>
2013-03-09 20:32:25 +0100

    autotests: DW

---
+++ em400/tests/alu/dw-div0.asm
@@ -0,0 +1,22 @@
+.program "alu/DW-div0"
+; 5/0
+; PRE [0xa0] = 0b0000000000000000
+; PRE [0xa1] =                   0b0000000000000101
+; PRE [0xa2] = 0b0000000000000000
+
+	ld 0xa0
+	dw 0xa2
+	rd 0xe0
+	rw r0, 0xe2
+
+	hlt 077
+
+.endprog
+
+; XPCT int(rz(6)) : 0
+; XPCT int(rz(7)) : 0
+; XPCT int(rz(8)) : 0
+; XPCT int(rz(9)) : 0
+; XPCT int(rz(10)) : 1
+; XPCT int(sr) : 0
+
---
+++ em400/tests/alu/dw-overflow.asm
@@ -0,0 +1,22 @@
+.program "alu/DW-overflow"
+; 5/0
+; PRE [0xa0] = 0b0111111111111111
+; PRE [0xa1] =                   0b1111111111111111
+; PRE [0xa2] = 0b0000000000000010
+
+	ld 0xa0
+	dw 0xa2
+	rd 0xe0
+	rw r0, 0xe2
+
+	hlt 077
+
+.endprog
+
+; XPCT int(rz(6)) : 0
+; XPCT int(rz(7)) : 1
+; XPCT int(rz(8)) : 0
+; XPCT int(rz(9)) : 0
+; XPCT int(rz(10)) : 0
+; XPCT int(sr) : 0
+
