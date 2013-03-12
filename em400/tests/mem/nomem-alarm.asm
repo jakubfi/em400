f8a5cc6f737eb11a54dc1de1c5e12cb3f5b84889
Jakub Filipowicz <jakubf@gmail.com>
2013-03-12 21:41:39 +0100

    tests: memory

---
+++ em400/tests/mem/nomem-alarm.asm
@@ -0,0 +1,15 @@
+.program "mem/nomem-alarm"
+
+; CONFIG configs/no_user_mem.cfg
+
+; PRE sr = 0b0000000000000001
+
+	pw r1, 10
+
+	hlt 077
+
+.endprog
+
+; XPCT int(rz(2)) : 1
+; XPCT int(rz(6)) : 0
+; XPCT int(alarm) : 1
