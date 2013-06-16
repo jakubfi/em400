498c163960e77b70018e880cd7b5843c9353d23a
Jakub Filipowicz <jakubf@gmail.com>
2013-06-16 20:42:53 +0200

    em400: tests: multix IWYZE test

---
+++ em400/tests/multix/iwyze_mcl.asm
@@ -0,0 +1,45 @@
+.prog "multix/IWYZE-mcl"
+
+; CONFIG configs/multix.cfg
+
+	.equ stackp 0x61
+	.equ stack 0x64
+	.equ prog_beg 0x70
+
+	.equ ints 0x40
+	.equ intsc ints + 12
+	.equ int_c3 intsc + 3
+
+	.equ unmask_chan 0b0000011110000000
+
+	UJ start
+
+	.ic stackp
+	.data stack
+	.ic int_c3
+	.data mx_int
+
+	.ic prog_beg
+mask:
+	.data unmask_chan
+mx_int:
+	AW r3, 1
+	LW r4, [stackp]
+	AWT r4, -1
+	LW r4, [r4]
+	LIP
+
+start:
+	LWT r3, 0
+	IM mask
+	MCL
+	IM mask
+	hlt 077
+
+.finprog
+
+; XPCT int(rz[15]) : 0
+; XPCT int(rz[6]) : 0
+; XPCT int(alarm) : 0
+; XPCT int(r3) : 2
+; XPCT bin(r4) : 0b0000001000000000
