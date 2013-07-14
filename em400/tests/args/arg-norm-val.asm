cbadb608d329dfd7ff8ec0e71a90b6fe86f96af5
Jakub Filipowicz <jakubf@gmail.com>
2013-07-14 15:24:44 +0200

    em400 tests: negative modifications

--- em400/tests/args/arg-norm-val.asm
+++ em400/tests/args/arg-norm-val.asm
@@ -2,10 +2,10 @@
 
 ; normal argument is a value
 
-	lw r1, 0x0001
-	lw r2, 0x0002
+	lw r1, 1
+	lw r2, 2
 	lw r3, r2
-	lw r4, r2 + 0x00f0
+	lw r4, r2 - 10
 	lw r5, r1 + r4
 	lw r6, r1 - 1
 	lw r7, r1 - 2
@@ -17,11 +17,11 @@
 ; XPCT int(rz[6]) : 0
 ; XPCT int(sr) : 0
 
-; XPCT hex(r1): 0x0001
-; XPCT hex(r2): 0x0002
-; XPCT hex(r3): 0x0002
-; XPCT hex(r4): 0x00f2
-; XPCT hex(r5): 0x00f3
-; XPCT hex(r6): 0x0000
-; XPCT hex(r7): 0xffff
+; XPCT int(r1): 1
+; XPCT int(r2): 2
+; XPCT int(r3): 2
+; XPCT int(r4): -8
+; XPCT int(r5): -7
+; XPCT int(r6): 0
+; XPCT int(r7): -1
 
