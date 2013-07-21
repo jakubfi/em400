73713225cb1eb0ff4d28aefd04c10ea6a0674f55
Jakub Filipowicz <jakubf@gmail.com>
2013-07-21 13:10:19 +0200

    em400 benchmarks

---
+++ em400/tests/benchmark/cycle.asm
@@ -0,0 +1,70 @@
+.prog "benchmark/cycle"
+
+	lw r1, -32000
+loop:
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	ujs 0
+	irb r1, loop
+	hlt 077
+
+.finprog
