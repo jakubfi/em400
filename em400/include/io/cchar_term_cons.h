a9bac8f13af0e3001d5e7a6a468b8a9e0b60ef0e
Jakub Filipowicz <jakubf@gmail.com>
2013-08-19 10:27:43 +0200

    em400: cchar/term_cons

---
+++ em400/include/io/cchar_term_cons.h
@@ -0,0 +1,56 @@
+//  Copyright (c) 2012-2013 Jakub Filipowicz <jakubf@gmail.com>
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+#ifndef CCHAR_TERM_CONS_H
+#define CCHAR_TERM_CONS_H
+
+#include "cchar.h"
+
+#include "cfg.h"
+
+struct cchar_unit_term_cons_t {
+    struct cchar_unit_proto_t proto;
+};
+
+// commands
+enum cchar_term_cmd_e {
+	// OU
+	CCHAR_TERM_CMD_RESET		= 0b100000, // reset
+	CCHAR_TERM_CMD_DISCONNECT	= 0b101000, // disconnect device (soft reset)
+	CCHAR_TERM_CMD_WRITE		= 0b110000, // write
+	// IN
+	CCHAR_TERM_CMD_SPU			= 0b100000, // check device
+	CCHAR_TERM_CMD_READ			= 0b101000, // read
+};
+
+// interrupts
+enum char_term_int_e {
+	CCHAR_TERM_INT_OUTDATED	= 0, // interrupt out of date
+	CCHAR_TERM_INT_READY	= 1, // ready again
+	CCHAR_TERM_INT_OPR_CALL	= 3, // operator call
+	CCHAR_TERM_INT_TOO_SLOW	= 5, // transmission too slow
+};
+
+struct cchar_unit_proto_t * cchar_term_cons_create(struct cfg_arg_t *args);
+void cchar_term_cons_shutdown(struct cchar_unit_proto_t *unit);
+void cchar_term_cons_reset(struct cchar_unit_proto_t *unit);
+void * cchar_term_cons_worker(void *th_id);
+int cchar_term_cons_cmd(struct cchar_unit_proto_t *unit, int dir, int cmd, uint16_t *r_arg);
+
+#endif
+
+// vim: tabstop=4 shiftwidth=4 autoindent
---
+++ em400/src/io/cchar_term_cons.c
@@ -0,0 +1,88 @@
+//  Copyright (c) 2013 Jakub Filipowicz <jakubf@gmail.com>
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <stdio.h>
+
+#include "errors.h"
+#include "cpu/memory.h"
+#include "io/cchar.h"
+#include "io/cchar_term_cons.h"
+
+#define UNIT ((struct cchar_unit_term_cons_t *)(unit))
+
+// -----------------------------------------------------------------------
+struct cchar_unit_proto_t * cchar_term_cons_create(struct cfg_arg_t *args)
+{
+	struct cchar_unit_term_cons_t *unit = calloc(1, sizeof(struct cchar_unit_term_cons_t));
+
+	if (!unit) {
+		goto fail;
+	}
+
+	return (struct cchar_unit_proto_t *) unit;
+
+fail:
+	cchar_term_cons_shutdown((struct cchar_unit_proto_t*) unit);
+	return NULL;
+}
+
+// -----------------------------------------------------------------------
+void cchar_term_cons_shutdown(struct cchar_unit_proto_t *unit)
+{
+	if (unit) {
+		free(UNIT);
+	}
+}
+
+// -----------------------------------------------------------------------
+void cchar_term_cons_reset(struct cchar_unit_proto_t *unit)
+{
+}
+
+// -----------------------------------------------------------------------
+int cchar_term_cons_cmd(struct cchar_unit_proto_t *unit, int dir, int cmd, uint16_t *r_arg)
+{
+	if (dir == IO_IN) {
+		switch (cmd) {
+		case CCHAR_TERM_CMD_SPU:
+			break;
+		case CCHAR_TERM_CMD_READ:
+			break;
+		default:
+			break;
+		}
+	} else {
+		switch (cmd) {
+		case CCHAR_TERM_CMD_RESET:
+			break;
+		case CCHAR_TERM_CMD_DISCONNECT:
+			break;
+		case CCHAR_TERM_CMD_WRITE:
+			printf("%c", *r_arg&255);
+			fflush(stdout);
+			break;
+		default:
+			break;
+		}
+	}
+	return IO_OK;
+}
+
+// vim: tabstop=4 shiftwidth=4 autoindent
