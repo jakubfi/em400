d9b3b4558544fd9b8720a430eb1450cdcb96f568
Jakub Filipowicz <jakubf@gmail.com>
2013-08-23 10:34:24 +0200

    em400: LOG() always logs Q, NB, IC

--- em400/src/debugger/log.c
+++ em400/src/debugger/log.c
@@ -27,6 +27,7 @@
 #include "cfg.h"
 #include "errors.h"
 #include "debugger/log.h"
+#include "cpu/cpu.h"
 
 pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
 
@@ -196,7 +197,7 @@ void log_log(int domain, int level, char *format, ...)
 	va_list vl;
 	va_start(vl, format);
 	pthread_mutex_lock(&log_mutex);
-	fprintf(log_f, "%s.%-6d %4s:%-3d ", now, (int) ct.tv_usec, log_dname[domain], level);
+	fprintf(log_f, "%s %4s @ %i:%i 0x%04x / ", now, log_dname[domain], SR_Q, SR_NB, cycle_ic);
 	vfprintf(log_f, format, vl);
 	fprintf(log_f, "\n");
 	pthread_mutex_unlock(&log_mutex);
