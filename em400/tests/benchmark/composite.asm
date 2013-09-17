025765e29faeb3ce5830c59b902b1895857e0f99
Jakub Filipowicz <jakubf@gmail.com>
2013-09-17 14:45:15 +0200

    em400: composite benchmark

---
+++ em400/tests/benchmark/composite.asm
@@ -0,0 +1,45 @@
+.prog "benchmark/composite"
+
+	.equ stackp 0x61
+	.equ soft 94
+	.equ start 0x70
+
+	uj start
+
+softx:
+	lip
+
+stack:	.res 4
+mask:	.data 0b0000000001000000
+one:	.data 1
+minus1:	.data -1
+
+.ic start
+	lwt r1, stack
+	rw r1, stackp
+	lwt r1, softx
+	rw r1, soft
+	im mask
+	lwt r1, 0
+
+loop:	lwt r2, -1		; short arg
+	md [minus1]		; d-mod, pre-mod
+	aw r3, r2 + 3		; b-mod, add, M-arg
+	ujs 0			; empty cycle
+	siu			; int serve
+	shc r3, 5		; shift
+	or r3, 0xbaba		; binary arithmetic
+	cw r3, [one]		; comparison
+	lb r3, one		; byte addressing
+	tw r3, one		; mem get
+	pw r3, one		; mem put
+	bb r3, 1		; P
+	ujs 0
+	rj r7, jump
+
+	irb r1, loop
+	hlt 077
+
+jump:	uj r7
+
+.finprog
