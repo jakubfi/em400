9e08842c6b339f72446b371a18c032b0cc433cbf
Jakub Filipowicz <jakubf@gmail.com>
2013-09-24 22:21:07 +0200

    em400: update tests according to emulation changes

--- em400/tests/alu/ad-minus-ov.asm
+++ em400/tests/alu/ad-minus-ov.asm
@@ -4,6 +4,7 @@
 
 	lw r1, 0b0111111111111111
 	aw r1, 0b0111111111111111
+	; =      1111111111111110
 	hlt 077
 
 .finprog
@@ -14,7 +15,7 @@
 ; XPCT int(r1) : -2
 ; Z, M, V, C
 ; XPCT int(r0[0]) : 0
-; XPCT int(r0[1]) : 1
+; XPCT int(r0[1]) : 0
 ; XPCT int(r0[2]) : 1
 ; XPCT int(r0[3]) : 0
 
--- em400/tests/alu/ad-ov-carry.asm
+++ em400/tests/alu/ad-ov-carry.asm
@@ -4,6 +4,7 @@
 
 	lw r1, 0b1111111111111111
 	aw r1, 0b1000000000000000
+	; =    1 0111111111111111
 	hlt 077
 
 .finprog
@@ -14,7 +15,7 @@
 ; XPCT int(r1) : 32767
 ; Z, M, V, C
 ; XPCT int(r0[0]) : 0
-; XPCT int(r0[1]) : 0
+; XPCT int(r0[1]) : 1
 ; XPCT int(r0[2]) : 1
 ; XPCT int(r0[3]) : 1
 
