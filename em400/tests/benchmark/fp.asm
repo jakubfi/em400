6c911fd644d1466357925262e5db22f64f61f05c
Jakub Filipowicz <jakubf@gmail.com>
2013-10-15 10:19:09 +0200

    em400 benchmark: fp

---
+++ em400/tests/benchmark/fp.asm
@@ -0,0 +1,30 @@
+.prog "benchmark/fp"
+
+	lw r7, -10000
+
+	lf d1m
+loop:
+	af d100
+	af d100
+	af d100
+	sf d100
+	af d100
+	sf d100
+	af d100
+	sf d100
+	df d100
+	mf d100
+	mf d0125
+	df d0125
+	mf d0125
+	af d1m
+	irb r7, loop
+
+	hlt 077
+
+d1m:	.data 0x7735, 0x9400, 0x001e ; 1000000000
+d100:	.data 0x9c00, 0x0000, 0x0007 ; -100
+d0125:	.data 0x4000, 0x0000, 0x00fe ; 0.125
+
+.finprog
+
