cee1e7c6859bd6c24ae49b2f20f2907ea5d5cb36
Jakub Filipowicz <jakubf@gmail.com>
2014-05-05 14:50:12 +0200

    another zc17 test

---
+++ tests/mod/zc17_neg.asm
@@ -0,0 +1,50 @@
+; CONFIG configs/mod.cfg
+
+; 17th bit for byte addressing is not set when
+; negative pre-mod causes C=1 when added to rB+rB
+
+	.cpu	mx16
+
+	.const	int_nomem 0x40 + 2
+	.const	stackp 0x61
+
+	.const	magic 0xfeba
+	.const	addr 0x200
+	.const	seg 4
+
+	uj	start
+
+mask:	.word	0b0100000000000000
+stack:	.res	16
+
+nomem_proc:
+	hlt	040
+
+	.org	0x70
+
+start:	lw	r1, stack
+	rw	r1, stackp
+	lw	r1, nomem_proc
+	rw	r1, int_nomem
+
+	lw	r1, seg\3 + 0\15
+	ou	r1, 3\10 + 0\14 + 1
+	.word	err, err, ok, err
+
+err:	hlt	040
+
+ok:	im	mask
+
+	lw	r1, magic
+	rw	r1, seg\3+addr-1
+
+	cron
+
+	lw	r7, seg\3+addr
+	md	-1
+	lb	r2, r7+r7
+
+	hlt	077
+
+; XPCT hex(r2) : 0x00ba
+; XPCT oct(ir[10-15]) : 077
