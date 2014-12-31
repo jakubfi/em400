e8c5567368686eee59f29f1cb8c6b21379a703c0
Jakub Filipowicz <jakubf@gmail.com>
2014-12-31 18:25:34 +0100

    H/W stack push/pop logging fix

--- src/cpu/cpu.c
+++ src/cpu/cpu.c
@@ -158,17 +158,30 @@ void cpu_reset()
 int cpu_ctx_switch(uint16_t arg, uint16_t ic, uint16_t sr_mask)
 {
 	uint16_t sp;
+
 	if (!mem_cpu_get(0, 97, &sp)) return 0;
+
+	LOG(L_CPU, 3, "H/W stack push @ 0x%04x (IC = 0x%04x, R0 = 0x%04x, SR = 0x%04x, arg = 0x%04x), new IC = 0x%04x",
+		sp,
+		regs[R_IC],
+		regs[0],
+		regs[R_SR],
+		arg,
+		ic
+	);
+
 	if (!mem_cpu_put(0, sp, regs[R_IC])) return 0;
 	if (!mem_cpu_put(0, sp+1, regs[0])) return 0;
 	if (!mem_cpu_put(0, sp+2, regs[R_SR])) return 0;
 	if (!mem_cpu_put(0, sp+3, arg)) return 0;
 	if (!mem_cpu_put(0, 97, sp+4)) return 0;
+
 	regs[0] = 0;
 	regs[R_IC] = ic;
 	regs[R_SR] &= sr_mask;
+
 	int_update_mask();
-	LOGCPU(L_CPU, 3, "ctx switch, IC = 0x%04x", ic);
+
 	return 1;
 }
 
@@ -181,13 +194,20 @@ int cpu_ctx_restore()
 	if (!mem_cpu_get(0, 97, &sp)) return 0;
 	if (!mem_cpu_get(0, sp-4, &data)) return 0;
 	regs[R_IC] = data;
-	LOGCPU(L_CPU, 3, "ctx restore, IC = 0x%04x", data);
 	if (!mem_cpu_get(0, sp-3, &data)) return 0;
 	regs[0] = data;
 	if (!mem_cpu_get(0, sp-2, &data)) return 0;
 	regs[R_SR] = data;
 	int_update_mask();
 	if (!mem_cpu_put(0, 97, sp-4)) return 0;
+
+	LOG(L_CPU, 3, "H/W stack pop @ 0x%04x (IC = 0x%04x, R0 = 0x%04x, SR = 0x%04x)",
+		sp-4,
+		regs[R_IC],
+		regs[0],
+		regs[R_SR]
+	);
+
 	return 1;
 }
 
