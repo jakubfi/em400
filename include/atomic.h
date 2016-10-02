42ca629dc16c22da4940ac2cc62736aa261e4270
Jakub Filipowicz <jakubf@gmail.com>
2016-10-02 12:19:17 +0200

    atomics rework start

--- include/atomic.h
+++ include/atomic.h
@@ -18,16 +18,33 @@
 #ifndef ATOMIC_H
 #define ATOMIC_H
 
-#if (defined __ATOMIC_SEQ_CST) && (defined __ATOMIC_RELAXED)
-	#define atom_load(ptr) __atomic_load_n(ptr, __ATOMIC_RELAXED)
-	#define atom_store(ptr, val) __atomic_store_n(ptr, val, __ATOMIC_RELAXED)
-	#define atom_fence() __atomic_thread_fence(__ATOMIC_SEQ_CST)
-#elif (defined __GCC_HAVE_SYNC_COMPARE_AND_SWAP_1) || (defined __GCC_HAVE_SYNC_COMPARE_AND_SWAP_2) || (defined __GCC_HAVE_SYNC_COMPARE_AND_SWAP_4) || (defined __GCC_HAVE_SYNC_COMPARE_AND_SWAP_8)
-	#define atom_load(ptr) __sync_fetch_and_or(ptr, 0)
-	#define atom_store(ptr, val) __sync_val_compare_and_swap(ptr, *ptr, val)
-	#define atom_fence() __sync_synchronize()
+// new GCC atomics (C++11-like)
+#if defined(__GNUC__) && (defined __ATOMIC_ACQUIRE) && (defined __ATOMIC_RELEASE) && (defined __ATOMIC_SEQ_CST)
+
+	#define atom_load(ptr)					__atomic_load_n(ptr, __ATOMIC_RELAXED)
+	#define atom_store(ptr, val)			__atomic_store_n(ptr, val, __ATOMIC_RELAXED)
+	#define atom_fence()					__atomic_thread_fence(__ATOMIC_SEQ_CST)
+
+	#define atom_load_acquire(ptr)			__atomic_load_n(ptr, __ATOMIC_ACQUIRE)
+	#define atom_store_release(ptr, val)	__atomic_store_n(ptr, val, __ATOMIC_RELEASE)
+	#define atom_full_fence()				__atomic_thread_fence(__ATOMIC_SEQ_CST)
+
+// "atomics" on x86
+#elif (defined (__x86_64__) || defined (__i386__))
+
+	#define atom_load(ptr)					*(ptr); asm volatile("" ::: "memory")
+	#define atom_store(ptr, val)			*(ptr) = (val); asm volatile("" ::: "memory")
+	#define atom_fence()					asm volatile("mfence" ::: "memory")
+
+	#define atom_load_acquire(ptr)			*(ptr); asm volatile("" ::: "memory")
+	#define atom_store_release(ptr, val)	*(ptr) = (val); asm volatile("" ::: "memory")
+	#define atom_full_fence()				asm volatile("mfence" ::: "memory")
+
+// other architectures and compilers
 #else
+
 	#error "Don't know how to handle atomic loads/stores with your compiler (and/or on your architecture)"
+
 #endif
 
 #endif
