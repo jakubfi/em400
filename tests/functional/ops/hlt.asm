d9f8e89026ca337f74b31d4337b5a3dcaf0d8561
Jakub Filipowicz <jakubf@gmail.com>
2017-03-25 21:40:15 +0100

    tests: more readable HLT test

--- tests/functional/ops/hlt.asm
+++ tests/functional/ops/hlt.asm
@@ -1,14 +1,16 @@
 ; PRECMD clock on
 
-	lw	r1, 0b0000100000000000
-	rw	r1, 20
+	lw	r1, stack
+	rw	r1, 0x61
 	lw	r1, fin
-	rw	r1, 69
-	im	20
-loop:	hlt	0
+	rw	r1, 0x40 + 5
+	im	mask
+loop:	hlt
 	ujs	loop
 
 fin:	hlt	077
+mask:	.word	0b0000100000000000
+stack:
 
 ; XPCT rz[6] : 0
 
