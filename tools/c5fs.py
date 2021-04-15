7cfe6b19229a02a5244a1877fb57b5bc85b48a21
Jakub Filipowicz <jakubf@gmail.com>
2021-04-15 18:49:09 +0200

    make c5fuse work

--- tools/c5fs.py
+++ tools/c5fs.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2013 Jakub Filipowicz <jakubf@gmail.com>
+#  Copyright (c) 2013, 2021 Jakub Filipowicz <jakubf@gmail.com>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
 #  Foundation, Inc.,
 #  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-from m400lib import r40, wload
+from m400lib import r40_str, wload
 
 # ------------------------------------------------------------------------
 class DDEntry:
@@ -23,17 +23,17 @@ class DDEntry:
     # --------------------------------------------------------------------
     def __init__(self, pos, data):
         self.pos = pos
-        self.name = "%s%s" % (r40(data[0]), r40(data[1]))
+        self.name = r40_str(data[0:2]).strip().lower()
         self.id = data[2]
         self.subdirs = data[3]
-        self.password = "%s" % r40(data[4])
+        self.password = r40_str(data[4:5])
         self.budget = data[5]
         self.topid = data[6]
         self.rights = data[7]
         self.suspended = True if data[7] & (1<<15) else False
         self.type = "User" if data[7] & (1<<14) else "Dir"
-        self.osl = "%s%s" % (r40(data[8]), r40(data[9]))
-        self.osl_arg = "%s%s" % (r40(data[10]), r40(data[11]))
+        self.osl = r40_str(data[8:10])
+        self.osl_arg = r40_str(data[10:12])
 
         # 0 = empty entry, skip
         if data[0] == 0:
@@ -52,8 +52,8 @@ class FDEntry:
     # --------------------------------------------------------------------
     def __init__(self, pos, data):
         self.pos = pos
-        self.name = "%s%s" % (r40(data[0]), r40(data[1]))
-        self.ext = "%s" % r40(data[3])
+        self.name = r40_str(data[0:2]).strip().lower()
+        self.ext = r40_str(data[3:4]).strip().lower()
         self.did = data[2]
         self.param1 = data[4]
         self.param2 = data[5]
@@ -68,7 +68,7 @@ class FDEntry:
         self.size = data[11]
 
         # skip GLOBAL in filesystem contents
-        if self.name.startswith("GLOBAL"):
+        if self.name.startswith("global"):
             raise ValueError
 
         # no top directory, ignore
@@ -104,13 +104,13 @@ class C5FS:
     # --------------------------------------------------------------------
     def read_label(self):
         data = wload(self.image, self.offset+self.label_offset, 17)
-        self.label = r40(data[0])
+        self.label = r40_str(data[0:1])
         self.dicdic_start = data[1]
         self.fildic_start = data[2]
         self.map_start = data[3]
         self.map_end = data[4]
         self.disk_end = data[5]
-        self.disk_name = "%s%s" % (r40(data[6]), r40(data[7]))
+        self.disk_name = r40_str(data[6:8]).lower()
         self.init_date = "%i-%i-%i" % (data[8], data[9], data[10])
         self.init_date_time = "%i-%i-%i %i:%i:%i" % (data[11], data[12], data[13], data[14], data[15], data[16])
 
@@ -168,10 +168,10 @@ class C5FS:
 
     # --------------------------------------------------------------------
     def read_map(self):
-        map_size = 512 * ((self.disk_end/8 + 512) / 512)
+        map_size = 512 * ((self.disk_end // 8 + 512) // 512)
         map_offset = 512 * (self.map_start + self.offset)
 
-        f = open(self.image, "r")
+        f = open(self.image, "rb")
         f.seek(map_offset)
         data = f.read(map_size)
         f.close()
@@ -179,7 +179,7 @@ class C5FS:
         self.map = []
         for byte in data:
             for bit in [7, 6, 5, 4, 3, 2, 1, 0]:
-                x = (ord(byte) >> 7) & 1
+                x = (byte >> 7) & 1
                 self.map.append(x)
 
     # --------------------------------------------------------------------
@@ -194,7 +194,7 @@ class C5FS:
         start = 512 * (self.offset + self.fildic[pos].start)
         size = 512 * (self.fildic[pos].size)
 
-        fin = open(self.image, "r")
+        fin = open(self.image, "rb")
         fin.seek(start)
         data = fin.read(size)
         fin.close()
--- tools/m400lib.py
+++ tools/m400lib.py
@@ -1,37 +1,47 @@
-#!/usr/bin/python
+import struct
 
 # ------------------------------------------------------------------------
 def r2a(i):
-	if (i>=1) and (i<=26):
-		return chr(i+64)
-	elif (i>=27) and (i<=36):
-		return chr(i+21)
-	elif i==37:
-		return "_"
-	elif i==38:
-		return "%"
-	elif i==39:
-		return "#"
-	elif i==0:
-		return " "
-	else:
-		return ""
+    if i >= 1 and i <= 26:
+        return chr(i + 64)
+    elif i >= 27 and i <= 36:
+        return chr(i + 21)
+    elif i == 37:
+        return "_"
+    elif i == 38:
+        return "%"
+    elif i == 39:
+        return "#"
+    elif i == 0:
+        return " "
+    else:
+        return ""
 
 # ------------------------------------------------------------------------
-def r40(w):
-	kz1 = r2a((w/1600) % 40)
-	kz2 = r2a((w/40) % 40)
-	kz3 = r2a(w % 40)
-	return "%s%s%s" % (kz1, kz2, kz3)
+def r40_triplet(w):
+    triplet = [
+        r2a((w // 1600) % 40),
+        r2a((w // 40) % 40),
+        r2a(w % 40),
+    ]
+    return ''.join(triplet)
+
+# ------------------------------------------------------------------------
+def r40_str(words):
+    chars = [r40_triplet(w) for w in words]
+    return ''.join(chars)
 
 # ------------------------------------------------------------------------
 def wload(ifile, sector, ilen):
-	f = open(ifile, "r")
-	f.seek(sector * 512)
-	odata = []
-	pos = 0
-	while pos<ilen:
-		odata.append(ord(f.read(1))*256 + ord(f.read(1)))
-		pos += 1;
-	f.close();
-	return odata
+    f = open(ifile, "rb")
+    f.seek(sector * 512)
+    odata = []
+    pos = 0
+    while pos < ilen:
+        data = struct.unpack('!H', f.read(2))[0]
+        odata.append(data)
+        pos += 1;
+    f.close();
+    return odata
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
