c25d89211ef3a127de10cd0924a0425311fec3e4
Jakub Filipowicz <jakubf@gmail.com>
2022-01-28 14:04:39 +0100

    properly use AR for group instructions, add benchmark

---
+++ tests/benchmark/synthetic/mem-multiword-3.asm
@@ -0,0 +1,35 @@
+	lwt	r1, 1
+loop:
+	lf	10
+	ll	10
+	lf	10
+	ll	10
+	lf	10
+	ll	10
+	lf	10
+	ll	10
+	lf	10
+	ll	10
+	lf	10
+	ll	10
+	lf	10
+	ll	10
+	lf	10
+	ll	10
+	lf	10
+	ll	10
+	lf	10
+	ll	10
+	lf	10
+	ll	10
+	lf	10
+	ll	10
+	lf	10
+	ll	10
+	lf	10
+	ll	10
+	lf	10
+	ll	10
+	irb	r1, loop
+	ujs	loop
+	hlt	077
---
+++ tests/benchmark/synthetic/mem-multiword-7.asm
@@ -0,0 +1,35 @@
+	lwt	r1, 1
+loop:
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	la	10
+	irb	r1, loop
+	ujs	loop
+	hlt	077
